2115f2126d0dceabd2e74d59484fe0eac10cee9e
Andrii Kozlov <kozlov.andrey2018@gmail.com>
2022-07-15 10:19:42 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 from presets.example import example_mail_preset
 from main.utils import send_many
 
-emails = [example_mail_preset('fortestskozlov@gmail.com', '', {}) for _ in range(5)]
+emails = [example_mail_preset('testmail@mail.com', '', {}) for _ in range(5)]
 send_many(emails)
